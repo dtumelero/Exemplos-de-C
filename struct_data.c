352a126ab1f5be0ad243864e9baa5bc62003b0d5
Diego Tumelero <dtumelero@users.noreply.github.com>
2017-10-17 19:59:50 -0200

    Exemplo de Structs

---
+++ struct_data.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//struct
+struct data{
+    char mes[10];
+	int  dia; 
+    int  ano;
+};
+
+void main(){
+    //declaração da struct de nome data_nascimento
+	struct data data_nascimento;		
+
+    printf("Digite o dia de nascimento: ");
+    scanf("%d", &data_nascimento.dia);
+
+    printf("Digite o mês de nascimento: ");
+    scanf("%s", data_nascimento.mes);	//quando é string não vai & no scanf
+
+    printf("Digite o ano de nascimento: ");
+    scanf("%d", &data_nascimento.ano);
+
+	//é possível dividir em multiplas linhas um comando
+	//mas é bom deixar bem "apresentado" para facilitar a leitura
+	printf("\nA data de nascimento é %d de %s de %d.\n", 
+			data_nascimento.dia, 
+			data_nascimento.mes, 
+			data_nascimento.ano);
+}
